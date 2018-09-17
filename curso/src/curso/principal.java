27af5eb1fde18e5829dc8936baef9dd47f7d677b
jonat <jonat@DESKTOP-RVJRGQV>
2018-09-17 00:56:33 -0300

    Pregunta el nombre y saluda.

---
+++ curso/src/curso/principal.java
@@ -0,0 +1,16 @@
+package curso;
+
+import javax.swing.JOptionPane;
+
+public class principal {
+
+	public static void main(String[] args) {
+		String nombre;
+		nombre = JOptionPane.showInputDialog("¿Cual es su nombre?");
+		
+		JOptionPane.showMessageDialog(null,"Buenos dias señor "+nombre);
+		
+		
+	}
+
+}
